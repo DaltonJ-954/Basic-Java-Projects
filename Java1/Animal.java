1af3b9ccb8e8f7ed2c231c579f409295d87f2f7e
Dalton Walls <scallywig@gmail.com>
2023-09-04 16:46:46 -0400

    edit

---
+++ Java1/Animal.java
@@ -0,0 +1,24 @@
+public class Animal {
+    public static String woodPecker() {
+        return "Peck Peck Peck!";
+    }
+
+    public static void doThings() {
+            // Try/Catch function
+            try {
+                int shops = 5;
+
+                if (shops == 10)
+                System.out.println("We made it");
+
+                else if(shops > 10)
+                System.out.println("We exceeded what we needed, YAY!");
+
+                else
+                System.out.println("We need to do better!");
+
+            } catch (Exception e) {
+                System.out.println("There is an error");
+            }
+        }
+    }
---
+++ Java1/LearnJava.java
@@ -0,0 +1,10 @@
+public class LearnJava {
+    public static void main(String[] args) {
+        Animal WP = new Animal();
+        String bird = WP.woodPecker();
+        System.out.println(bird);
+
+        Animal a = new Animal();
+        a.doThings();
+    }
+}
