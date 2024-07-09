b4e2daa7ce0f812ddda4cdbc9256feef99196896
Dalton Walls <scallywig@gmail.com>
2024-07-09 17:23:10 -0400

    edits

---
+++ Java1/main.kt
@@ -0,0 +1,13 @@
+
+
+fun main() {
+    var Kia = 28
+    var KJ = 23
+    var Kianna = 26
+    if (KJ == Kia) {
+        println("KJ has caught up to his oldest sister in age! Unreal...")
+    }
+    else if (KJ > Kianna) {
+        println("KJ is can never be older than his sister!")
+    }
+}
